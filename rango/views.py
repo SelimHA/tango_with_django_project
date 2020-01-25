aacd62444830b0cdf666633514a2c2819d490eae
Selim Hadjadj-Aoul <2396794h@student.gla.ac.uk>
2020-01-25 17:02:07 +0000

    Chapter 6 finished

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
+from rango.models import Page
 
 # Create your views here.
-def index(request):
-        context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-        return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
         context_dict = {'your_name': 'Selim Hadjadj-Aoul'}
         return render(request, 'rango/about.html', context=context_dict)
+def index(request):
+        # Make query, order database by likes (desc) and only show top 5
+        category_list = Category.objects.order_by('-likes')[:5]
+        context_dict = {}
+        context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+        context_dict['categories'] = category_list
+        # Render response
+        return render(request, 'rango/index.html', context=context_dict)
+def show_category(request, category_name_slug):
+        context_dict = {}
+        try:
+                # Get model of category or raise exception if doesnt exist
+                category = Category.objects.get(slug=category_name_slug)
+                # Get all associated pages in list
+                pages = Page.objects.filter(category=category)
+                context_dict['pages'] = pages
+                context_dict['category'] = category
+        except Category.DoesNotExist:
+                # Category doesnt exist so dont do anything
+                context_dict['category'] = None
+                context_dict['pages'] = None
+        # Go render the response and return it to the client.
+        return render(request, 'rango/category.html', context=context_dict)
